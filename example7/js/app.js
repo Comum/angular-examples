fee465b557db8ac29c04b74cddd55e814ce1a95e
Comum <miguel.rib.20@gmail.com>
2018-05-22 22:55:54 +0100

    Create example 7

---
+++ example7/js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('myApp', []);
+
+app.controller('MyController', function ($scope, $parse) {
+    $scope.$watch('expr', function (newVal, oldVal, scope) {
+        if (newVal !== oldVal) {
+            var parseFun = $parse(newVal);
+
+            $scope.parsedValue = parseFun(scope);
+        }
+    });
+});
