98560a75a739e75658a6ad20cdca5c1974da33ba
Comum <miguel.rib.20@gmail.com>
2018-05-26 01:18:02 +0100

    Continue with example 9

--- example9/js/app.js
+++ example9/js/app.js
@@ -1,23 +1,58 @@
-angular.module('myApp', [])
-    .directive('ensureUnique', function($http) {
-        return {
-            require: 'ngModel',
-            link: function (scope, ele, attrs, c) {
-                scope.$watch(attrs.ngModel, function (n) {
-                    if (!n) {
-                        return;
-                    }
+var app = angular.module('myApp', [])
 
-                    $http({
-                        method: 'POST',
-                        url: '/api/check/' + attrs.ensureUnique,
-                        data: { 'field': attrs.ensureUnique }
-                    }).success(function (data) {
-                        c.$setValidity('unique', data.isUnique);
-                    }).error(function (data) {
-                        c.$setValidity('unique', false);
-                    });
+app.directive('ensureUnique', function($http) {
+    return {
+        require: 'ngModel',
+        link: function (scope, ele, attrs, c) {
+            scope.$watch(attrs.ngModel, function (n) {
+                if (!n) {
+                    return;
+                }
+
+                $http({
+                    method: 'POST',
+                    url: '/api/check/' + attrs.ensureUnique,
+                    data: { 'field': attrs.ensureUnique }
+                }).success(function (data) {
+                    c.$setValidity('unique', data.isUnique);
+                }).error(function (data) {
+                    c.$setValidity('unique', false);
                 });
-            }
+            });
         }
-    });
+    }
+});
+
+app.controller('signupController', ['$scope', function ($scope) {
+    $scope.submitted = false;
+    $scope.signupForm = function () {
+        if ($scope.signup_form.$valid) {
+            // submit as normal
+        } else {
+            $scope.signup_form.submitted = true;
+        }
+    }
+}]);
+
+app.directive('ngFocus', [function () {
+    var FOCUS_CLASS = 'ng-focused';
+
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function ($scope, element, attrs, ctrl) {
+            ctrl.$focused = false;
+            element.bind('focus', function (evt) {
+                element.addClass(FOCUS_CLASS);
+                $scope.$apply(function () {
+                    ctrl.$focused = true;
+                });
+            }).bind('blur', function (evt) {
+                element.removeClass(FOCUS_CLASS);
+                scope.$apply(function () {
+                    ctrl.$focused = false;
+                });
+            })
+        }
+    };
+}]);
