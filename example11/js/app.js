364c1d0e97cdeb7e15684afc9b3a10fe61e6f8a6
Comum <miguel.rib.20@gmail.com>
2018-05-26 01:35:49 +0100

    Created example 11

---
+++ example11/js/app.js
@@ -0,0 +1,11 @@
+angular.module('myApp', [])
+    .run(function ($rootScope) {
+        $rootScope.rootProperty = 'root scope';
+    })
+    .controller('ParentCtrl', function ($scope) {
+        $scope.parentProperty = 'parent scope';
+    })
+    .controller('ChildCtrl', function ($scope) {
+        $scope.childProperty = 'child scope';
+        $scope.fullSentenceFromChild = 'Same $scope: We can access: ' + $scope.rootProperty + ' and ' + $scope.parentProperty + ' and ' + $scope.childProperty;
+    });
