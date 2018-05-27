437b2d354dcaf1c1cc2b3430cbc616e3f27b2b2d
Comum <miguel.rib.20@gmail.com>
2018-05-27 19:53:07 +0100

    Updating index page

--- mainApp/app.js
+++ mainApp/app.js
@@ -1,30 +1,75 @@
 var pageList = angular.module('mainPage', []);
+var pageMultiplier = 5;
 
-pageList.controller('mainCtrl', function ($scope) {
-    var i;
-
-    $scope.pages = [];
-
-    console.log($scope)
+function generatePagesLinks() {
+    var links = [];
 
     for (i = 1 ; i <= 11 ; i++) {
-        $scope.pages.push({
+        links.push({
             route: '/example' + i,
             name: 'Example ' + i
         });
     }
 
+    return links;
+}
+
+pageList.controller('mainCtrl', function ($scope) {
+    var i;
+
+    $scope.pages = generatePagesLinks();
+    $scope.pagesIndex = 0;
+    $scope.maxPagesIndex = Math.ceil($scope.pages.length / pageMultiplier) - 1;
+    $scope.minPageReached = true;
+    $scope.maxPageReached = false;
+
     function getPagesInView(index) {
-        return $scope.pages.slice(index * 5, (index + 1) * 5);
+        return $scope.pages.slice(index * pageMultiplier, (index + 1) * pageMultiplier);
     }
 
-    $scope.pagesInView = getPagesInView(0);
+    function updatePagesInView() {
+        $scope.pagesInView = getPagesInView($scope.pagesIndex);
+    }
+
+    updatePagesInView();
 
     $scope.updateName = function () {
         if (this.name.length) {
             $scope.pagesInView = $scope.pages;
         } else {
-            $scope.pagesInView = getPagesInView(0);
+            updatePagesInView();
         }
     };
-})
+
+    $scope.decreasePage = function () {
+        $scope.maxPageReached = false;
+
+        if ($scope.pagesIndex > 0) {
+            $scope.pagesIndex = $scope.pagesIndex - 1;
+        }
+
+        if ($scope.pagesIndex === 0) {
+            $scope.minPageReached = true;
+        } else {
+            $scope.minPageReached = false;
+        }
+
+        updatePagesInView();
+    };
+
+    $scope.increasePage = function () {
+        $scope.minPageReached = false;
+
+        if ($scope.pagesIndex < $scope.maxPagesIndex) {
+            $scope.pagesIndex = $scope.pagesIndex + 1;
+        }
+
+        if ($scope.pagesIndex === $scope.maxPagesIndex) {
+            $scope.maxPageReached = true;
+        } else {
+            $scope.maxPageReached = false;
+        }
+
+        updatePagesInView();
+    };
+});
